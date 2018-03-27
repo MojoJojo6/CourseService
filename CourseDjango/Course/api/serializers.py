71e203fb13fed176e82dbbcc6dca6a22a447aac8
Raghav Mittal <raghavmittal101@gmail.com>
2018-03-27 15:43:49 +0530

    FIX - lesson typo

--- CourseDjango/Course/api/serializers.py
+++ CourseDjango/Course/api/serializers.py
@@ -15,6 +15,7 @@ class Lesson_itemSerializer(serializers.ModelSerializer):
     lesson_item_name        = serializers.CharField(max_length=20)
     lesson_item_seqname     = serializers.IntegerField()
     lesson_item_description = serializers.CharField(max_length=200)
+    lesson_item_asset_link =  serializers.CharField(max_length=200)
     date_created            = serializers.DateTimeField(read_only=True)
     date_modified           = serializers.DateTimeField(read_only=True)
 
@@ -25,6 +26,7 @@ class Lesson_itemSerializer(serializers.ModelSerializer):
             'lesson_item_name',
             'lesson_item_seqname',
             'lesson_item_description',
+            'lesson_item_asset_link',
             'date_created',
             'date_modified'
         ]
@@ -40,7 +42,7 @@ class BulkLessonSerializer(serializers.ModelSerializer):
     lesson_description  = serializers.CharField(max_length=200)
     date_created        = serializers.DateTimeField(read_only=True)
     date_modified       = serializers.DateTimeField(read_only=True)
-    lession_items       = Lesson_itemSerializer(many=True, read_only=True)
+    lesson_items       = Lesson_itemSerializer(many=True, read_only=True)
 
     class Meta:
         model = Lesson
